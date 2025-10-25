4063b922602c89278f2b2829a4fd0ced8aefb5ec
OblakoTeamUA <tzspjkbgjc@privaterelay.appleid.com>
2025-10-25 18:44:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ const products = {
   hookahs: [
     { id: 1, name: "Odin Hookah", price: 8950, image: "images/odinmini.png" },
     { id: 2, name: "Karma 1.0", price: 8550, image: "images/karma01.png" },
-    { id: 3, name: "Sky Hookah SDM Purple", price: 3350, image: "images/sky purpl.png" }
-    { id: 4, name: "Totem Mikra", price: 4900, image: "images/sky purpl.png" }
+    { id: 3, name: "Sky Hookah SDM Purple", price: 3350, image: "images/sky purpl.png" },
+    { id: 4, name: "Totem Mikra", price: 4900, image: "images/sky purpl.png" },
+    { id: 3, name: "Tiaga Black Edition", price: 3350, image: "images/sky purpl.png" }
   ],
   heat: [
     { id: 4, name: "TCL", price: 1050, image: "images/tclb.png" },
